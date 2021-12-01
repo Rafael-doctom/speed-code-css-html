51d0105075502e892454915cce437a7a1a71c7fc
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2021-12-01 12:17:38 -0300

    -- start App

--- 07-crud/main.js
+++ 07-crud/main.js
@@ -6,6 +6,20 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: "Rafael",
+    email: "rafael@gmail.com",
+    celular: "19292922",
+    cidade: "TATUI"
+}
+
+
+// CRUD - create read update delet
+const createClient = (client) => {
+    localStorage.setItem("db_client", JSON.stringify(client))
+}
+
+// eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
